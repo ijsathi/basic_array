3a009d697c1e263247ab3f27112b69882b3b7aea
unknown <ijsathi2001rs@gmail.com>
2023-10-11 16:28:12 +0600

    reverse arry

---
+++ array.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main()
+{
+    int ar[5];
+    ar[0] = 5;
+    ar[1] = 10;
+    ar[2] = 15;
+    ar[3] = 20;
+    ar[4] = 25;
+    printf("%d", ar[4] );
+    return 0;
+}
---
+++ arry_initialize.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main()
+{
+    int arry[50] = {2,4,6,8,10};
+    for(int i=0; i<50 ;i++)
+    {
+         printf("%d\n", arry[i]);
+    }
+    return 0;
+}
